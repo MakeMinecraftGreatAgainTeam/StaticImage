39d83130003116d037b8c3c0030d8b9c7d99d83b
wzp <minecraftwzpmc@163.com>
2023-05-13 18:20:11 +0800

    Init

---
+++ src/main/java/cn/wzpmc/staticimage/StaticImage.java
@@ -0,0 +1,17 @@
+package cn.wzpmc.staticimage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaticImage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
